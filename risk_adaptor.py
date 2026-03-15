22e099148492cacc83449b0e2410677f36f773d6
dmjxzf8xqf-a11y <dhy316000@naver.com>
2026-03-15 13:43:59 +0900

    Update risk_adaptor.py

--- risk_adaptor.py
+++ risk_adaptor.py
@@ -1,8 +1,20 @@
 # risk_adaptor.py
-
-def adjust_risk(base_risk, daily_pnl):
-    if daily_pnl > 0:
-        return base_risk * 1.2
-    elif daily_pnl < -2:
-        return base_risk * 0.7
-    return base_risk
+def adapt_position_size(base_usdt: float, enter_score: float, regime: str) -> float:
+    size = float(base_usdt)
+    if regime == "high_volatility":
+        size *= 0.45
+    elif regime == "sideways":
+        size *= 0.80
+    elif regime in ("bull_strong", "bear_strong"):
+        size *= 1.00
+    if enter_score >= 85:
+        size *= 1.20
+    elif enter_score >= 75:
+        size *= 1.00
+    elif enter_score >= 68:
+        size *= 0.75
+    else:
+        size *= 0.50
+    size = max(size, base_usdt * 0.30)
+    size = min(size, base_usdt * 1.25)
+    return round(size, 4)
